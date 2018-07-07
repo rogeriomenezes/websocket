96f8f49257d6c256c15bd8069d38c75625d1459e
Tiago Felipe <tiago@tiagofelipe.com>
2018-07-07 15:25:40 -0300

    Dev

--- index.js
+++ index.js
@@ -17,7 +17,32 @@ const wss = new WebSocket.Server({
 wss.on('connection', function connection (ws, req) {
   ws.on('message', function incoming (message) {
     console.log('received: %s', message);
+    const ip = req.connection.remoteAddress;
+    console.log(ip)
+    //console.log(req.connection)
+    //console.log(req.session)
+    //console.log(req)
   });
+  
+  ws.on('listening', function listening (t) {
+    console.log('Listening...')
+  })
 
-  // ws.send('algo');
+  ws.on('close', function listening (t, a) {
+    console.log('Connection close', t, a)
+    wss.clients.forEach(function each(client) {
+      console.log('Client ', client)
+    })
+  })
+
+  ws.send(['teste', 'algo']);
 });
+
+const interval = setTimeout(function ping() {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send('teste', {test: 1});
+      //console.log(client.session)
+    }
+  });
+}, 10000);
