3db60fbfd18727d46fa258073139c2fde0f6b6fc
Tiago Felipe <tiago@tiagofelipe.com>
2018-07-10 14:26:32 -0300

    Dev

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const WebSocket = require('ws');
+let env = 'production';
+
+process.argv.forEach((val, index) => {
+  if (val === '--dev') {
+    env = 'dev';
+  }
+});
+
+log(`Start WebSocket Server in ${env} environment`)
 
 const wss = new WebSocket.Server({
   verifyClient: (info, done) => {
-    console.log('Parsing session from request...');
-    console.log('Session is parsed!');
+    log('Try to parsing session from request...');
+
+    let logged = true;
+    log('Session is parsed!');
+
     //
     // We can reject the connection by returning false to done(). For example,
-    // reject here if user is unknown.
+    // reject here if user is unknown or token is invalid.
     //
-    done(true);
+    done(logged);
 
   },
   port: 8444
@@ -16,54 +28,35 @@ const wss = new WebSocket.Server({
 
 wss.on('connection', function connection (ws, req) {
   const ip = req.connection.remoteAddress;
-  console.log('Connection open from ', ip)
+  log('Connection open from ', ip)
   ws.on('message', function incoming (message) {
-    console.log('received: %s', message);
-    //console.log(req.connection)
-    //console.log(req.session)
-    //console.log(req)
+    log('received: %s', message);
+    //log(req.connection)
+    //log(req.session)
+    //log(req)
   });
 
   ws.on('listening', function listening (t) {
-    console.log('Listening...')
+    log('Listening...')
   })
 
   ws.on('open', function listening (t, a) {
     const ip = req.connection.remoteAddress;
-    console.log('Connection open from ', ip)
+    log('Connection open from ', ip)
   })
 
   ws.on('close', function listening (t, a) {
-    console.log('Connection close', t, a)
+    log('Connection close', t, a)
   })
-
-  ws.send('Hello Brother, i am a websocket server :)');
 });
 
-const interval = setInterval(function ping () {
-  wss.clients.forEach(function each (client) {
-    if (client.readyState === WebSocket.OPEN) {
-      let data = {
-        type: 'lance',
-        data: {
-          leilao: 1,
-          lote: {
-            id: 10,
-            lance: {
-              id: 200,
-              data: '2018-07-10 09:01:27',
-              valor: 13200.00,
-              arrematante: {
-                id: 50,
-                apelido: 'TIAGOFELIPE',
-                localidade: 'Montes Claros - MG'
-              }
-            }
-          }
-        }
-      }
-      client.send(JSON.stringify(data));
-      //console.log(client.session)
-    }
-  });
-}, 5000);
+/**
+ * Register an log
+ * @param m
+ * @param level 0 = Simple. For Dev env | 1 = Important. For Any Env
+ */
+const log = function (m, level) {
+  level = typeof level !== 'undefined' ? level : 0;
+  if (env === 'production' && level === 0) return;
+  console.log(m);
+}
